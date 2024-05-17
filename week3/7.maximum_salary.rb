c5408a7b00c16a227527f7f6da10f5eb6734538f
Abdullah Said <asaid@nxt.so>
2024-05-17 17:08:23 +0300

    1.binary_search solution

---
+++ week3/7.maximum_salary.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+# maximum salary problem
---
+++ week4/1.binary_search.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# maximum number of prizes problem
+def binary_search(array, low, high, number)
+  if high >= low
+    mid = low + (high - low) / 2
+    guess = array[mid]
+    if guess == number
+      mid
+    elsif guess < number
+      binary_search array, mid + 1, high, number
+    else
+      binary_search array, low, mid - 1, number
+    end
+  else
+    -1
+  end
+end
+
+def binary_search_array(array, low, high, search_array)
+  result = []
+  search_array.each do |number|
+    result << binary_search(array, low, high, number)
+  end
+  result.join(' ')
+end
+
+
+# getting input from coursera
+input = gets("\t\n").chomp.split("\n")
+n1 = input[0].to_i
+array = input[1].split.map(&:to_i)
+_ = input[2].to_i
+search_array = input[3].split.map(&:to_i)
+
+print binary_search_array array, 0, n1 - 1, search_array

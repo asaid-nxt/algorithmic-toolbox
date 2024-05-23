f5947a6fc9a9cf4929c62c050c2585f25d176f1c
Abdullah Said <asaid@nxt.so>
2024-05-23 13:54:09 +0300

    1.money_change update solution

--- week3/1.money_change.rb
+++ week3/1.money_change.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 # money change problem
-def change(money, denominations: [10, 5, 1])
+def change(money, coins)
   return 0 if money.zero?
 
-  denominations.shift until denominations.first <= money
-  max = denominations.first
-  1 + change(money - max, denominations: denominations)
+  coin = coins.select { |c| c <= money }.first
+  1 + change(money - coin, coins)
 end
 
 input = gets.chomp.to_i
-print change input
+print change input, [10, 5, 1]

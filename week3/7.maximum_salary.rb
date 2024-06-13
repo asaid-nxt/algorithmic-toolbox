9dba302785706df52f96e4255b5f28079dfe78b7
Abdullah Said <asaid@nxt.so>
2024-06-13 20:17:00 +0300

    7.maximum_salary solution

--- week3/7.maximum_salary.rb
+++ week3/7.maximum_salary.rb
@@ -1,3 +1,21 @@
 # frozen_string_literal: true
 
 # maximum salary problem
+def max_salary(nums)
+  nums = nums.map(&:to_s)
+
+  sorted_nums = nums.sort do |a, b|
+    (b + a) <=> (a + b)
+  end
+
+  largest_num = sorted_nums.join
+  largest_num[0] == '0' ? '0' : largest_num
+end
+
+
+
+# Input
+n = gets.to_i
+nums = gets.split.map(&:to_i)
+
+puts max_salary(nums)

fb442fe31c3ac65e9101827242d8576f23f036df
Abdullah Said <asaid@nxt.so>
2024-06-13 19:48:01 +0300

    6.max_number_of_prizes solution

--- week3/6.max_number_of_prizes.rb
+++ week3/6.max_number_of_prizes.rb
@@ -1,3 +1,24 @@
 # frozen_string_literal: true
 
 # maximum number of prizes problem
+def max_number_of_prizes(n)
+  k = 1
+  current_sum = 0
+  numbers = []
+
+  while current_sum + k <= n
+    current_sum += k
+    numbers << k
+    k += 1
+  end
+
+  numbers[-1] += n - current_sum if current_sum < n
+
+  [numbers.size, numbers]
+end
+
+input = gets.chomp.to_i
+k, numbers = max_number_of_prizes(input)
+puts k
+puts numbers.join(' ')
+

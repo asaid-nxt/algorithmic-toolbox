81f13d370e77758bde95eb2d1d6ec8b9829bfc01
Abdullah Said <asaid@nxt.so>
2024-06-13 19:39:15 +0300

    5.fibonacci_number_again solution

--- week2/5.fibonacci_number_again.rb
+++ week2/5.fibonacci_number_again.rb
@@ -1,4 +1,32 @@
 # frozen_string_literal: true
 
 # fibonacci number again problem (huge number)
+def pisano_period(m)
+  prev = 0
+  curr = 1
+  (0...(m * m)).each do |i|
+    prev, curr = curr, (prev + curr) % m
+    return i + 1 if prev.zero? && curr == 1
+  end
+end
+
+def fibonacci_mod(n, m)
+  pisano = pisano_period(m)
+  n %= pisano
+
+  return n if n <= 1
+
+  prev = 0
+  curr = 1
+  (n - 1).times do
+    prev, curr = curr, (prev + curr) % m
+  end
+  curr
+end
+
+# input
+input = gets.chomp.split.map(&:to_i)
+n = input[0]
+m = input[1]
+print fibonacci_mod n, m
 
